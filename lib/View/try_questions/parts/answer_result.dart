940ca676fa05761c593a1a32120c574cfac16b0a
fen0268 <Kenshi150107@gmail.com>
2023-01-07 21:34:14 +0900

    ファイル名を変更、パス整理

--- lib/View/try_questions/parts/answer_result.dart
+++ lib/View/try_questions/parts/answer_result.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:past_questions/View/tryquestions/tryquestions.dart';
 
-class AnswerResult extends ConsumerWidget{
+import '../try_questions.dart';
+
+class AnswerResult extends ConsumerWidget {
+  const AnswerResult({super.key});
 
   @override
-  Widget build(context,ref){
+  Widget build(BuildContext context, WidgetRef ref) {
     final questionP = ref.watch(questionProvider);
     final quizAnswer = questionP.model.quizAnswer;
 
     return Center(
-      child: quizAnswer != "" ? 
-        Text(quizAnswer) :
-        null,
+      child: quizAnswer != '' ? Text(quizAnswer) : null,
     );
   }
 }
