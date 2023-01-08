51ac082725e9a9b5633389e90a080cfa33b113b4
asahaya <asa7kadensony@gmail.com>
2023-01-09 03:01:53 +0900

    Question部分提案_追加3

--- lib/questions/question.dart
+++ lib/questions/question.dart
@@ -30,26 +30,26 @@ class Question {
   int year_question; //年度の？問目
   String season; //春or秋
   String category; //3大カテゴリ
-  String category_Detail; //3大カテゴリ内の詳細 ストラテジorマネジメントor
+  String category_Detail; //3大カテゴリ内の詳細 ストラテジorマネジメントorテクノロジ
   bool afterCheck; //後で確認したい問題
 
-  String question_statement; //問題文
-  String question_imagePath; //問題図
+  String q_statement; //問題文
+  String q_imagePath; //問題図
 
   //　↓　ここはあってもなくてもいい（負荷が掛かるならいらない）
-  int all_q_count; //全体の回答数
-  int all_q_correct; //全体の正解数
-  double correct_per; //問題の歴代全体正解率　＝全体の正解数/全体の回答数
+  int all_q_count; //全ユーザーの回答数
+  int all_q_correct; //全ユーザーの正解数
+  double correct_per; //問題の歴代全体ユーザー正解率　＝全体の正解数/全体の回答数
   int retry_count; //この問題何回目？
   //　↑
 
   List<Map<String, dynamic>> choices = [
     //アの選択
     {
-      'answer_text': '',
-      'answer_image': '',
-      'keyword': '',
-      'judge': '',
+      'answer_text': '', //選択肢 アプリ上表記文
+      'answer_image': '', //選択肢 図（図がある場合は基本、文はなし）
+      'keyword': '', //この選択肢にかかわる単語
+      'judge': '', //正解or不正解
     },
     //イの選択
     {
@@ -83,11 +83,13 @@ class Question {
     required this.category,
     required this.category_Detail,
     this.afterCheck = false,
-    required this.question_statement,
-    this.question_imagePath = "",
+    required this.q_statement,
+    this.q_imagePath = "",
     //選択肢の表記??
     required this.choices,
     required this.correct_per,
     required this.retry_count,
+    required this.all_q_correct,
+    required this.all_q_count,
   });
 }
