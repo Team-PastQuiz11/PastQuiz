8efda6c033dfc7fd52df432832b93d79592c5395
Rai <faid156498@gmail.com>
2023-01-13 08:52:56 +0900

    竹内　提案

--- lib/questions/question2.dart
+++ lib/questions/question2.dart
@@ -1,15 +1,15 @@
-//1つのdartファイルに過去問データを全部書くテイで
+//以下は竹内のご提案です。
 class Question2 {
   int year; //年度　<-[竹内] 静的(変化しない)部分　class管理でオッケー
-  int year_question; //年度の？問目　<-[竹内]  静的
-  String season; //春or秋　<-[竹内]  静的
-  String category; //3大カテゴリ　<-[竹内]  静的
-  String category_Detail; //3大カテゴリ内の詳細 ストラテジorマネジメントorテクノロジ <-[竹内]  静的
+  int year_question; //年度の？問目　<-[竹内]  静的 
+  int season; //春or秋　<-[竹内]  0-なし 1-春 2-秋　intで管理すれば問題作成時のミスが防ぎやすいと考えています
+  int category; //3大カテゴリ　<-[竹内]  0 1 2
+  int category_Detail; //3大カテゴリ内の詳細 ストラテジorマネジメントorテクノロジ <-[竹内]  静的　
 
   String q_statement; //問題文　<-[竹内]  静的
   String q_imagePath; //問題図　<-[竹内]  静的
 
-  //[竹内] 選択肢に関しては静的な部分が大半だと考える。keywordだけ不明
+  //[竹内] 選択肢に関しては静的な部分が大半だと考える。keywordはリンク？
   List<Chioce> choices ;
 
   Question2({
@@ -24,6 +24,13 @@ class Question2 {
     required this.choices,
   });
 }
+
+//出力する際の条件分岐の例
+const Map<int,String> seazonMap = {
+  0:"",
+  1:"春",
+  2:"秋"
+};
 
 class Chioce{
 
@@ -40,15 +47,29 @@ class Chioce{
   });
 }
 
-// final List<Question2> quizListTest = [
-//   Question2(
-//     year: year, 
-//     year_question: year_question, 
-//     season: season, 
-//     category: category, 
-//     category_Detail: category_Detail, 
-//     q_statement: q_statement, 
-//     choices: [
-//       {},
-//     ])
-// ]
+
+//実際に問題作成するときの想定
+final List<Question2> quizListTest = [
+  Question2(
+    year: 2022, 
+    year_question: 1, 
+    season: 0, 
+    category: 0, 
+    category_Detail: 0, 
+    q_statement:  """
+著作権及び特許権に関する記述a~cのうち,適切なものだけを全て挙げたものはどれか。
+
+a 偶然二つの同じようなものが生み出された場合， 発明に伴う特許権は両方に認められるが， 著作権は一方の著作者にだけ認められる。
+b ソフトウェアの場合， 特許権も著作権もソースプログラムリストに対して認められる。
+c 特許権の取得には出願と登録が必要だが， 著作権は出願や登録の必要はない。
+    """, 
+    choices: [
+      Chioce(answer_text: "a,b",judge: false,),
+      Chioce(answer_text: "b",  judge: false,),
+      Chioce(answer_text: "b,c",judge: false,),
+      Chioce(answer_text: "c",judge: true,),
+    ]
+  ),
+
+
+];
