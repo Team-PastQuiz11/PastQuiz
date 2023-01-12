1e7c86dfc5213e4a028c4b957240b1a99c7697b4
Rai <faid156498@gmail.com>
2023-01-13 00:13:11 +0900

    takerai comments

--- lib/questions/question.dart
+++ lib/questions/question.dart
@@ -26,30 +26,31 @@ import 'package:past_questions/questions/2022/questions_2022.dart';
 
 //1つのdartファイルに過去問データを全部書くテイで
 class Question {
-  int year; //年度
-  int year_question; //年度の？問目
-  String season; //春or秋
-  String category; //3大カテゴリ
-  String category_Detail; //3大カテゴリ内の詳細 ストラテジorマネジメントorテクノロジ
-  bool afterCheck; //後で確認したい問題
+  int year; //年度　<-[竹内] 静的(変化しない)部分　class管理でオッケー
+  int year_question; //年度の？問目　<-[竹内]  静的
+  String season; //春or秋　<-[竹内]  静的
+  String category; //3大カテゴリ　<-[竹内]  静的
+  String category_Detail; //3大カテゴリ内の詳細 ストラテジorマネジメントorテクノロジ <-[竹内]  静的
+  bool afterCheck; //後で確認したい問題 <-[竹内] 　ローカル管理?(もし他のユーザーと共有したいのであればオンライン検討)
 
-  String q_statement; //問題文
-  String q_imagePath; //問題図
+  String q_statement; //問題文　<-[竹内]  静的
+  String q_imagePath; //問題図　<-[竹内]  静的
 
   //　↓　ここはあってもなくてもいい（負荷が掛かるならいらない）
-  int all_q_count; //全ユーザーの回答数
-  int all_q_correct; //全ユーザーの正解数
-  double correct_per; //問題の歴代全体ユーザー正解率　＝全体の正解数/全体の回答数
-  int retry_count; //この問題何回目？
+  int all_q_count; //全ユーザーの回答数　<-[竹内]  firebase
+  int all_q_correct; //全ユーザーの正解数　<-[竹内]  firebase
+  double correct_per; //問題の歴代全体ユーザー正解率　＝全体の正解数/全体の回答数　<-[竹内]  firebase
+  int retry_count; //この問題何回目？　<-[竹内]  よくわかってない。静的？
   //　↑
 
-  List<Map<String, dynamic>> choices = [
+  //[竹内] 選択肢に関しては静的な部分が大半だと考える。keywordだけ不明
+  List<Map<String, dynamic>> choices = [ 
     //アの選択
     {
       'answer_text': '', //選択肢 アプリ上表記文
       'answer_image': '', //選択肢 図（図がある場合は基本、文はなし）
       'keyword': '', //この選択肢にかかわる単語
-      'judge': '', //正解or不正解
+      'judge': '', //正解or不正解　
     },
     //イの選択
     {
