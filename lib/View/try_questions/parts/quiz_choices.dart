aaec5cd37d80023f7c6cbf351715de373c52f9cf
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-01-18 11:21:47 +0900

    :art: issue #29 問題選択肢ボタンのサイズ、配置変更
    - サイズ  : 横幅を画面両端から16px隙間を開けた幅に、縦幅を選択しテキストに応じたサイズに変更。
    - 配置 : 選択肢同士の上下感覚を0.5px開け、窮屈な配置を解消。ボタンテキストを左寄せに。

--- lib/View/try_questions/parts/quiz_choices.dart
+++ lib/View/try_questions/parts/quiz_choices.dart
@@ -37,25 +37,42 @@ class QuizChoicesState extends ConsumerState<QuizChoices> {
     return Column(
       children: [
         for (var i = 0; i < 4; i++) ...[
-          ElevatedButton(
-            onPressed: quizAnswer == ''
-                ? () {
-                    questionP.quizAnswerChange(
-                      i == popQuestion.answer ? '正解' : '不正解',
-                    );
+          Container(
+            // 横幅指定　左右に16px、上下に0.5pxの隙間を開ける
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 0.5),
+            width: 500,
+            // 選択肢ボタンを横の揃える
+            alignment: Alignment.centerLeft,
+            child: ElevatedButton(
+              onPressed: quizAnswer == ''
+                  ? () {
+                      // 回答選択肢が問題の正解だった場合'正解'、そうでない場合'不正解'を返す三項演算子
+                      questionP.quizAnswerChange(
+                        i == popQuestion.answer ? '正解' : '不正解',
+                      );
 
-                    specificRecord.isNotEmpty
-                        ? UserRecordsModel.updateRecord(
-                            popQuestionStr,
-                            i == popQuestion.answer ? 0 : 1,
-                          )
-                        : UserRecordsModel.createRecord(
-                            popQuestionStr: popQuestionStr,
-                            isCorrect: i == popQuestion.answer ? 0 : 1,
-                          );
-                  }
-                : null,
-            child: Text(popQuestion.choices[i]),
+                      // 成績の有無によってDBに成績を作成するか、既存の成績を更新するかの三項演算子
+                      specificRecord.isNotEmpty
+                          ? UserRecordsModel.updateRecord(
+                              popQuestionStr,
+                              i == popQuestion.answer ? 0 : 1,
+                            )
+                          : UserRecordsModel.createRecord(
+                              popQuestionStr: popQuestionStr,
+                              isCorrect: i == popQuestion.answer ? 0 : 1,
+                            );
+                    }
+                  // quizAnswerが空ではなかった場合の処理は無い
+                  // ここもう少しスマートに書けたりしないかな？
+                  : null,
+              child: Container(
+                // ボタンの文字を左寄りに
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  popQuestion.choices[i],
+                ),
+              ),
+            ),
           )
         ],
       ],
