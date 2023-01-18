6d2c1101a1594e410bd2e4aaf0fea2d37d3ccaec
Takatoshi-ish <ishiguro.taka@gmail.com>
2023-01-18 20:03:37 +0900

    投稿画面UI作成

--- lib/View/Chat/edit_page.dart
+++ lib/View/Chat/edit_page.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class EditPage extends StatelessWidget {
+class EditPage extends ConsumerStatefulWidget {
   const EditPage({super.key});
 
   @override
+  ConsumerState<EditPage> createState() => EditPageState();
+}
+
+class EditPageState extends ConsumerState<EditPage> {
+  final TextEditingController _targetTextController = TextEditingController();
+
+  @override
   Widget build(context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("投稿ページの予定"),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(15.0),
+        child: TextField(
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          controller: _targetTextController,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'ここに入力',
+            alignLabelWithHint: true,
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.clear),
+              onPressed: () {
+                _targetTextController.clear();
+              },
+            ),
+          ),
+          autofocus: true,
+        ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        elevation: 7.0,
+        onPressed: null,
+        child: Text('投稿'),
       ),
     );
   }
