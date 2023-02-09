ca471c1f81da345fb1f8745e277c457ad4c721d6
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-02-09 11:16:03 +0900

    :+1:　issue #48 firebase接続を意識したスレッド表示ロジック、スレッドタイルをriverpodで実装
    🚩 やらなかったこと （次回やる）
    ・`lib/view/chat/chat_screen.dart`の画面遷移修正（エラー出たまんま）
    ・デザイン面の修正
    ・provider、consumerWidgetのファイル分類（責務分離）

--- lib/view/chat/chat_thread_screen.dart
+++ lib/view/chat/chat_thread_screen.dart
@@ -1,65 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../router/appbar/custom_appbar.dart';
 
-class ChatThreadScreen extends StatefulWidget {
-  const ChatThreadScreen({super.key});
+// class ChatThreadScreen extends StatefulWidget {
+//   const ChatThreadScreen({super.key});
 
-  @override
-  State<ChatThreadScreen> createState() => _ChatThreadScreenState();
+//   @override
+//   State<ChatThreadScreen> createState() => _ChatThreadScreenState();
+// }
+
+// class _ChatThreadScreenState extends State<ChatThreadScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: const CustomAppbar(),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: ListView(
+//               children: [
+//                 for (var i = 0; i < 100; i++) ...[
+//                   Container(
+//                     margin: const EdgeInsets.symmetric(horizontal: 7),
+//                     decoration: const BoxDecoration(
+//                       border: Border(bottom: BorderSide(color: Colors.grey)),
+//                     ),
+//                     child: ListTile(
+//                       title: const Text('タイトル'),
+//                       subtitle: const Text('サブタイトル'),
+//                       onTap: () {},
+//                     ),
+//                   ),
+//                 ],
+//               ],
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(20),
+//             child: Row(
+//               children: [
+//                 const Flexible(
+//                   child: TextField(
+//                     decoration: InputDecoration(
+//                       enabledBorder: OutlineInputBorder(),
+//                       focusedBorder: OutlineInputBorder(),
+//                     ),
+//                     maxLines: 10,
+//                     minLines: 1,
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.only(left: 10),
+//                   child: OutlinedButton(
+//                     onPressed: () {},
+//                     child: const Text('投稿'),
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// ↓↓↓↓↓ まだ責務分離をしていない ↓↓↓↓↓
+
+// スレッドタイルのモデル
+class Message {
+  final User user;
+  final String text;
+
+  Message({required this.user, required this.text});
 }
 
-class _ChatThreadScreenState extends State<ChatThreadScreen> {
+class User {
+  final String name;
+  final String avatar;
+
+  User({required this.name, required this.avatar});
+}
+
+// スレッド一覧画面のプロバイダー
+final messagesProvider = StateNotifierProvider((ref) => MessagesController());
+
+// ここでスレッド一覧画面の状態管理
+class MessagesController extends StateNotifier<List<Message>> {
+  MessagesController() : super([]);
+
   @override
-  Widget build(BuildContext context) {
+  // MessagesPageクラスで`state`を参照するためのgetterを定義
+  List<Message> get state => super.state;
+
+  void addMessage(Message message) {
+    state = [...state, message];
+  }
+}
+
+// 画面表示本体
+class MessagesPage extends ConsumerWidget {
+  const MessagesPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final messagesController = ref.watch(messagesProvider) as MessagesController;
+    final messages = messagesController.state;
+
     return Scaffold(
       appBar: const CustomAppbar(),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: [
-                for (var i = 0; i < 100; i++) ...[
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 7),
-                    decoration: const BoxDecoration(
-                      border: Border(bottom: BorderSide(color: Colors.grey)),
-                    ),
-                    child: ListTile(
-                      title: const Text('タイトル'),
-                      subtitle: const Text('サブタイトル'),
-                      onTap: () {},
-                    ),
-                  ),
-                ],
-              ],
+      body: ListView.builder(
+        itemCount: messages.length,
+        itemBuilder: (context, index) {
+          final message = messages[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(message.user.avatar),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                const Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(),
-                    ),
-                    maxLines: 10,
-                    minLines: 1,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: const Text('投稿'),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+            title: Text(message.user.name),
+            subtitle: Text(message.text),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add a message
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
